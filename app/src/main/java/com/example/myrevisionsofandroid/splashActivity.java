03c5e967ec65fc8388e131b196a211cc91f0988c
Muhammad Bilal <muhammadbilal20130@gmail.com>
2021-12-24 09:28:01 +0500

    this is temp git

---
+++ app/src/main/java/com/example/myrevisionsofandroid/splashActivity.java
@@ -0,0 +1,34 @@
+package com.example.myrevisionsofandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(splashActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        thread.start();
+        Toast.makeText(this, "this ", Toast.LENGTH_LONG).show();
+    }
+
+}
