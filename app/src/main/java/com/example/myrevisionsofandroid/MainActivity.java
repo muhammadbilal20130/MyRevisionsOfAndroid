bb3de82c750799a46b1d34edeb0b0afb8c0c09c8
Muhammad Bilal <muhammadbilal20130@gmail.com>
2021-12-27 18:59:44 +0500

    Adding toast

--- app/src/main/java/com/example/myrevisionsofandroid/MainActivity.java
+++ app/src/main/java/com/example/myrevisionsofandroid/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myrevisionsofandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView TvLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TvLogin = findViewById(R.id.tvUserLogin);
+        TvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "This is my revison of andorid", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        });
+
     }
 }
